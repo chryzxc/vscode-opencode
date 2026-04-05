16d3bb5367e26d566581eaf5dc7b51f2ae85967e
Christian Rey Villablanca <chriztiann.villablanca@gmail.com>
2026-04-05 15:55:57 +0800

    chore: delete files

--- fix_test_file.mjs
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-const content = fs.readFileSync('tests/components/plan-detection.test.mjs', 'utf8');
-const fixed = content.replace(/extractFunctionBody\(([^,]+),\s*'([^']+)',\s*\);/g, 'extractFunctionBody($1, \'$2\');');
-fs.writeFileSync('tests/components/plan-detection.test.mjs', fixed);
